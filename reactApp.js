0ce59a786ce2f2a97522b99eaab30f95d4bd83a0
mist <mist@Abbys-MacBook-Pro.local>
2021-02-24 16:30:49 -0500

    done with part one

--- reactApp.js
+++ reactApp.js
@@ -3,15 +3,58 @@ function App(props){
   return (
     <div>
       <h1>Welcome to the sports game starter</h1>
+      <Team />
     </div>
+    
   )
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+class Team extends React.Component {
+  constructor( props ) {
+    super( props )
+    this.state = {
+      score: 0,
+      shots: 0,
+      shotPercentage: 0,
+    }
+  }
+
+  shootFunction = () => {
+    let missSound = new Audio( "miss.wav" );
+    let hitSound = new Audio( "hit.wav" );
+    
+    if (getRandomInt(5)) {
+      this.setState((state) => ({
+        score: state.score + 1,
+      } ) );
+      hitSound.play();
+    }
+    this.setState( ( state ) => ( {
+      shots: state.shots + 1
+    } ) )
+    missSound.play();
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <p>Shots Taken: {this.state.shots}</p>
+        <p>Team Score: {this.state.score}</p>
+        <button onClick={this.shootFunction}>Shot</button>
+      </div>
+    );
+  }
+
+}
+  
 //Render the application
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
-// I haven't finished this, but turning it in before the deadline, 
-// and plan to work on this today still. 
